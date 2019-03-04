a90df87bff3a2a3b302fbf49a9de10722f6380dd
guantao21 <guantao21@gmail.com>
2019-03-04 21:50:30 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
+define('APIKEY', AIzaSyA3Jrza_hCLYb1Sv3cMYoLOvuur42bkpG0("APIKEY"));
+define('GJ_CODE', us("GJ_CODE"));
+define('SITE_NAME',gtyoutube("SITE_NAME"));
+define('TITLENAME', gtyoutube("TITLENAME"));
 define('EN2DEKEY',getenv("EN2DEKEY"));
 define('EMAIL', getenv("EMAIL"));
 ?>
